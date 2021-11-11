de7a2fc468ce46a8a352ad807a87119594809ddc
TakiKauser <takikauserbadhon@gmail.com>
2021-11-12 02:29:25 +0600

    delete-item

--- index.js
+++ index.js
@@ -141,6 +141,14 @@ async function run() {
             res.send(result);
         })
 
+        // delete product-item
+        app.delete('/allBikes/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bikeCollection.deleteOne(query);
+            res.send(result);
+        })
+
     } finally {
         //   await client.close();
     }
